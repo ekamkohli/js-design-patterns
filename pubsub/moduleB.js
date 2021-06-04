c4905a57a9a51dcb6d3c055b94b38c3b76a3f4e6
Ekampreet Singh <ekampreet.singh@block8.com>
2021-06-05 02:09:12 +0530

    call unsubscribe method after subscribing

--- pubsub/moduleB.js
+++ pubsub/moduleB.js
@@ -1,6 +1,7 @@
 // The subscriber module
 const pubSub = require("./pubsub");
 
-pubSub.subscribe("anEvent", (data) => {
+const subscription = pubSub.subscribe("anEvent", (data) => {
   console.log(`"anEvent", was published with the data: "${data.msg}"`);
+  subscription.unsubscribe();
 });
